abedacd1f252a1fcdfbddf807f059f100ed2d0ff
B-litljon <brandonC.littlejohn@gmail.com>
2024-01-29 05:49:32 -0800

    Add Plotly graphing functionality

--- utils/websocket_handler.py
+++ utils/websocket_handler.py
@@ -1,10 +1,11 @@
 from binance import Client, ThreadedWebsocketManager
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 import os
 import time
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from indicators.bollinger_bands import BollingerBands
 from indicators.rsi import RSI
 from indicators.sup_res import SupportResistance
@@ -20,6 +21,41 @@ api_secret = os.getenv('BINANCE_SECRET_KEY')
 bbands = BollingerBands(window=20, num_of_std=2)
 rsi_indicator = RSI(period=14)
 sup_res = SupportResistance(window=14)
+
+
+# Global DataFrame to accumulate data
+data_df = pd.DataFrame(columns=['Time', 'Open', 'High', 'Low', 'Close', 'RSI', 'UpperBB', 'MiddleBB', 'LowerBB', 'Support', 'Resistance'])
+
+# Initialize Plotly figure
+fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.2, subplot_titles=('Candlesticks with Bollinger Bands', 'RSI'))
+fig.add_trace(go.Candlestick(x=[], open=[], high=[], low=[], close=[], name='Candlesticks'), row=1, col=1)
+fig.add_trace(go.Scatter(x=[], y=[], mode='lines', name='Upper Band'), row=1, col=1)
+fig.add_trace(go.Scatter(x=[], y=[], mode='lines', name='Middle Band'), row=1, col=1)
+fig.add_trace(go.Scatter(x=[], y=[], mode='lines', name='Lower Band'), row=1, col=1)
+fig.add_trace(go.Scatter(x=[], y=[], mode='lines', name='RSI'), row=2, col=1)
+fig.update_layout(xaxis_rangeslider_visible=False, height=800)
+
+def update_plot():
+    fig.data[0].x = data_df['Time']
+    fig.data[0].open = data_df['Open']
+    fig.data[0].high = data_df['High']
+    fig.data[0].low = data_df['Low']
+    fig.data[0].close = data_df['Close']
+
+    fig.data[1].x = data_df['Time']
+    fig.data[1].y = data_df['UpperBB']
+
+    fig.data[2].x = data_df['Time']
+    fig.data[2].y = data_df['MiddleBB']
+
+    fig.data[3].x = data_df['Time']
+    fig.data[3].y = data_df['LowerBB']
+
+    fig.data[4].x = data_df['Time']
+    fig.data[4].y = data_df['RSI']
+
+    fig.show()
+
 
 #client = Client(api_key, api_secret, tld='us')
 # Function to handle incoming WebSocket messages for K-line data
