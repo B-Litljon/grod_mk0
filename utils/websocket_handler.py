5fb9c687941a2f8ff949073eb82eff6dc6a07925
B-litljon <brandonC.littlejohn@gmail.com>
2024-02-08 01:52:22 -0800

    Refactor BinanceWebsocketStream class and start method

--- utils/websocket_handler.py
+++ utils/websocket_handler.py
@@ -15,38 +15,34 @@ from .indicators.rsi import RSI
 from .indicators.sup_res import SupportResistance
 from .trade_logic import TradeCalculator, TradeConfig
 
-def start_websocket_stream(api_key, api_secret, symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_1MINUTE):
-    # Initialize indicators within the function if they don't need to be accessed outside
-    bbands = BollingerBands(window=20, num_of_std=2)
-    rsi_indicator = RSI(period=14)
-    sup_res = SupportResistance(window=14)
+class BinanceWebsocketStream:
+    def __init__(self, symbol, interval, api_key, secret_key):
+        self.symbol = symbol
+        self.interval = interval
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.bbands = BollingerBands(window= 20, stddev=2)
+        self.rsi = RSI(period=14)
+        self.dataframe = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'rsi', 'UpperBB', 'MiddleBB', 'LowerBB'])
+        self.twm = ThreadedWebsocketManager(api_key=self.api_key, api_secret=self.secret_key, tld='us')
 
-    # DataFrame to accumulate data
-    data_df = pd.DataFrame(columns=['Time', 'Open', 'High', 'Low', 'Close', 'RSI', 'UpperBB', 'MiddleBB', 'LowerBB', 'Support', 'Resistance'])
-
-    def handle_socket_message(msg):
-        nonlocal data_df # Referencing global DataFrame
-        print("Message received")  # Debugging print
-        sys.stdout.flush()  # Flush output buffer
-        # Extract the candlestick data from the message
-        candlestick = msg['k']
+    def handle_socket_message(self, msg):
+        print('message received')
+        sys.stdout.flush()
+        candle = msg['k']
         rsi_value = None
-        
-        # Extracting relevant information from the candlestick data
-        open_price = float(candlestick['o'])
-        high_price = float(candlestick['h'])
-        low_price = float(candlestick['l'])
-        close_price = float(candlestick['c'])
-        volume = float(candlestick['v'])
-        close_time = pd.to_datetime(candlestick['T'], unit='ms')
-        
-        # Update indicators     
-        if hasattr(bbands, 'prices') and len(bbands.prices) > 0:
-            previous_close = bbands.prices[-1]
-            rsi_value = rsi_indicator.update(close_price, previous_close)
+        open_price = float(candle['o'])
+        high_price = float(candle['h'])
+        low_price = float(candle['l'])
+        close_price = float(candle['c'])
+        close_time = pd.to_datetime(candle,['T'], unit='ms')
 
-        upper_band, middle_band, lower_band = bbands.update(close_price)
-        support, resistance = sup_res.update(high_price, low_price)
+        if hasattr(self.bbands, 'prices') and len (self.bbands.prices) > 0:
+            previous_close = self.bbands.prices[-1]
+            rsi_value = self.rsi_indicator.update(close_price, previous_close)
+
+        upper_band, middle_band, lower_band = self.bbands.update(close_price)
+
 
         new_data = {
             'Time': close_time,
@@ -57,42 +53,26 @@ def start_websocket_stream(api_key, api_secret, symbol='BTCUSDT', interval=Clien
             'RSI': rsi_value if rsi_value is not None else np.nan,
             'UpperBB': upper_band,
             'MiddleBB': middle_band,
-            'LowerBB': lower_band,
-            'Support': support,
-            'Resistance': resistance
+            'LowerBB': lower_band
         }
-
+        
         # Efficiently append new data to DataFrame
-        data_df_length = len(data_df)
-        data_df.loc[data_df_length] = new_data
+        data_df_length = len(self.dataframe)
+        self.dataframe.loc[data_df_length] = new_data
 
         # Limit DataFrame size
         max_rows = 1000
         if data_df_length > max_rows:
-            data_df.drop(data_df.index[0], inplace=True)
+            self.dataframe.drop(self.dataframe.index[0], inplace=True)
 
-        # Dynamic console output
-        output = (
-            f"Candlestick Data: Open: {new_data['Open']}, High: {new_data['High']}, Low: {new_data['Low']}, Close: {new_data['Close']}\n"
-            f"RSI: {new_data['RSI']}\n"
-            f"BB Data: Upper: {new_data['UpperBB']}, Middle: {new_data['MiddleBB']}, Lower: {new_data['LowerBB']}\n"
-            f"Support: {new_data['Support']}, Resistance: {new_data['Resistance']}\n"
-        )
-
-        # Clear the console and print the output
-        sys.stdout.write("\033[H\033[J")  # Clear screen and move to home position
-        sys.stdout.write(output)
-        sys.stdout.flush()
-    # Initialize and start the WebSocket manager
-    twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret, tld='us')
-    twm.start()
-    stream_name = twm.start_kline_socket(symbol=symbol, interval=interval, callback=handle_socket_message)
-
-    try:
-        while True:
-            time.sleep(1)  # Keep the function running to listen for messages
-    except KeyboardInterrupt:
-        twm.stop_socket(stream_name)
-        twm.join()
+    def start(self):
+        self.twm.start()
+        stream_name = self.twm.start_kline_socket(symbol=self.symbol, interval=self.interval, callback=self.handle_socket_message)
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.twm.stop_socket(stream_name)
+            self.twm.join()
 
 
