c8a20b4e57a69569b41f3d2c689520f4ae81dfd4
B-litljon <brandonC.littlejohn@gmail.com>
2024-04-06 19:30:02 -0700

    Refactor Triggers class to use NumPy arrays for improved performance and memory usage

--- utils/signals/trigger.py
+++ utils/signals/trigger.py
@@ -1,52 +1,12 @@
 from utils.indicators.bollinger_bands import BollingerBands
 from utils.indicators.rsi import RSI
 import logging
-import pandas as pd
+import numpy as np
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 class Triggers:
-    """
-A class for implementing trading strategies based on technical analysis indicators such as Bollinger Bands and RSI.
-
-Instantiate the class by creating an instance of Triggers:
-    triggers = Triggers(bollinger_bands, rsi_values, price_data)
-
-Args:
-    bollinger_bands (BollingerBands): An instance of the BollingerBands class containing the upper, middle, and lower bands.
-    rsi_values (list): A list of RSI values.
-    price_data (list): A list of dictionaries containing price data with keys 'open', 'close', 'high', and 'low'.
-
-The `Triggers` class provides methods to check for specific market conditions and generate trading signals based on
-various strategies involving RSI, Bollinger Bands, and candlestick patterns.
-
-Available methods:
-    - `rsi_divergence_strategy()`: To be implemented for RSI divergence strategy.
-    - `is_bullish_engulfing()`: Checks for a bullish engulfing pattern in the latest two candles.
-    - `rsi_and_bb_expansion_strategy()`: Checks for a trading signal based on RSI values, Bollinger Band expansion, and
-                                         bullish engulfing pattern.
-
-The `rsi_and_bb_expansion_strategy()` method implements a two-stage strategy:
-    1. Stage 1: Checks if the price is below the lower Bollinger Band and RSI is oversold (below 25).
-    2. Stage 2: If Stage 1 is triggered, checks if RSI is back in the normal range (between 30 and 35),
-                Bollinger Bands are expanding (based on the ROC of the bandwidth), and a bullish engulfing pattern is present.
-
-If both stages are satisfied, the method returns True, indicating a potential trading signal.
-
-Example usage:
-    # Create instances of BollingerBands and Triggers
-    bollinger_bands = BollingerBands(window=20, num_of_std=2)
-    triggers = Triggers(bollinger_bands, rsi_values, price_data)
-    
-    # Check for trading signals
-    if triggers.rsi_and_bb_expansion_strategy():
-        # Execute trade based on the signal
-        pass
-
-Note: The `rsi_divergence_strategy()` method is not yet implemented and needs to be defined based on the specific
-      requirements of the RSI divergence strategy.
-"""
     def __init__(self, bollinger_bands, rsi_values, price_data):
         self.bollinger_bands = bollinger_bands
         self.rsi_values = rsi_values
