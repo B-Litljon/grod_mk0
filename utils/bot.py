910eddf16ec41f4f347b048b285c3dbddbaba405
B-litljon <brandonC.littlejohn@gmail.com>
2024-04-08 19:23:23 -0700

    updated Bot class gets rid of the 'process_kline' method

--- utils/bot.py
+++ utils/bot.py
@@ -12,16 +12,16 @@ from utils.signals.trigger import Triggers
 from utils.safety.order_calculation import OrderCalculator, TradeConfig
 
 class Bot:
-    def __init__(self, symbol, interval, api_key , api_secret):
+    def __init__(self, symbol, interval, api_key, api_secret):
         self.symbol = symbol
         self.interval = interval
         self.api_key = api_key
         self.api_secret = api_secret
-        self.bbands = BollingerBands(window= 20, num_of_std=2)
+        self.bbands = BollingerBands(window=20, num_of_std=2)
         self.rsi = RSI(period=14)
-        self.kline_data = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'rsi', 'UpperBB', 'MiddleBB', 'LowerBB'])
+        self.kline_data = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'rsi', 'UpperBB', 'MiddleBB', 'LowerBB'])
         self.twm = ThreadedWebsocketManager(api_key=self.api_key, api_secret=self.api_secret, tld='us')
-        self.order_calculator = OrderCalculator(TradeConfig()) # 'tc' is an instance of OrderCalculator class renamed in this case to 'OrderCalculator'
+        self.order_calculator = OrderCalculator(TradeConfig())  # 'tc' is an instance of OrderCalculator class renamed in this case to 'OrderCalculator'
         self.client = Client(api_key=self.api_key, api_secret=self.api_secret, tld='us')
         self.trigger = Triggers(bollinger_bands=self.bbands, rsi_values=self.rsi, price_data=self.kline_data)
 
@@ -29,46 +29,53 @@ class Bot:
         self.logger = logging.getLogger(__name__)
 
     def fetch_historical_data(self):
-        self.logger.info('fetching historical data')
-        klines = self.client.get_historical_klines(symbol=self.symbol, interval=self.interval, limit= 100)
+        self.logger.info('Fetching historical data')
+        klines = self.client.get_historical_klines(symbol=self.symbol, interval=self.interval, limit=100)
         for kline in klines:
-            self.append_data_to_df(kline)
+            kline_data = {
+                'time': pd.to_datetime(kline[0], unit='ms'),
+                'open': float(kline[1]),
+                'high': float(kline[2]),
+                'low': float(kline[3]),
+                'close': float(kline[4]),
+                'volume': float(kline[5]),
+                'close_time': pd.to_datetime(kline[6], unit='ms')
+            }
+            self.append_data_to_df(kline_data)
 
     def append_data_to_df(self, kline):
-        open_time, open_price, high_price, low_price, close_price, volume, close_time = self.process_kline(kline)
         # Calculate indicators
         previous_price = self.kline_data['close'].iloc[-1] if len(self.kline_data) > 0 else 0
-        rsi_value = self.rsi.update(close_price, previous_price)
-        upper_band, middle_band, lower_band = self.bbands.update(close_price)
-        # Append new data to the DataFrame
-        new_data = {'time': close_time, 'open': open_price, 'high': high_price, 'low': low_price, 'close': close_price, 'volume': volume, 'rsi': rsi_value, 'upperbb': upper_band, 'middlebb': middle_band, 'lowerbb': lower_band}
+        rsi_value = self.rsi.update(kline['close'], previous_price)
+        upper_band, middle_band, lower_band = self.bbands.update(kline['close'])
+        # Append new data to the DataFrame using loc
+        new_data = {
+            'time': kline['time'],
+            'open': kline['open'],
+            'high': kline['high'],
+            'low': kline['low'],
+            'close': kline['close'],
+            'volume': kline['volume'],
+            'rsi': rsi_value,
+            'upperbb': upper_band,
+            'middlebb': middle_band,
+            'lowerbb': lower_band
+        }
         self.kline_data.loc[len(self.kline_data)] = new_data
         self.logger.info(f"New data appended to DataFrame: {new_data}")
-       
-    def process_kline(self, kline):
-        open_time = pd.to_datetime(kline[0], unit='ms')
-        open_price = float(kline[1])
-        high_price = float(kline[2])
-        low_price = float(kline[3])
-        close_price = float(kline[4])
-        volume = float(kline[5])
-        close_time = pd.to_datetime(kline[6], unit='ms')
-        self.logger.debug(f"Processed kline data: {kline}")
-        return open_time, open_price, high_price, low_price, close_price, volume, close_time
-        
 
     def handle_socket_message(self, msg):
         self.logger.info('Message received')
         kline = msg['k']
-        kline_data = np.array([
-            kline['t'],  # Open time
-            kline['o'],  # Open price
-            kline['h'],  # High price
-            kline['l'],  # Low price
-            kline['c'],  # Close price
-            kline['v'],  # Volume
-            kline['T']   # Close time
-        ])
+        kline_data = {
+            'time': pd.to_datetime(kline['t'], unit='ms'),
+            'open': float(kline['o']),
+            'high': float(kline['h']),
+            'low': float(kline['l']),
+            'close': float(kline['c']),
+            'volume': float(kline['v']),
+            'close_time': pd.to_datetime(kline['T'], unit='ms')
+        }
         self.append_data_to_df(kline_data)
         self.check_signal()
         max_rows = 101
@@ -86,17 +93,17 @@ class Bot:
             self.order_calculator.buy_order(
                 symbol=self.symbol,
                 order_type='market',
-                quantity=self.order_calculator.calculate_order_size(entry_price=self.kline_data['close'].iloc[-1]), # calculate order size needs to also calculate stop loss and take profit 
+                quantity=self.order_calculator.calculate_order_size(entry_price=self.kline_data['close'].iloc[-1]),  # calculate order size needs to also calculate stop loss and take profit
                 newClientOrderId='test_order').manage_orders(closing_price=self.kline_data['close'].iloc[-1])
-            
+
     def start(self):
         self.fetch_historical_data()
         self.twm.start()
         stream_name = self.twm.start_kline_socket(
-            symbol=self.symbol, 
-            interval=self.interval, 
+            symbol=self.symbol,
+            interval=self.interval,
             callback=self.handle_socket_message
-        ) # need to call fetch historical data here to fill the dataframe b4 the ws stream starts
+        )  # need to call fetch historical data here to fill the dataframe b4 the ws stream starts
         try:
             while True:
                 time.sleep(1)
